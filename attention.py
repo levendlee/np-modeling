1d578c21b896df7f12289a1efac7d987d635e957
levendlee <levendlee@hotmail.com>
2023-12-13 23:31:09 -0800

    Fixes bugs in attention backward path.

--- attention.py
+++ attention.py
@@ -10,15 +10,28 @@ import mlp
 
 class Softmax(mlp.Activation):
     def forward(self, x: np.ndarray) -> np.ndarray:
+        self._x = x
 
         x_max = np.max(x, axis=-1, keepdims=True)
         exp_x = np.exp(x - x_max)
         exp_x_sum = np.sum(exp_x, axis=-1, keepdims=True)
-        return exp_x / exp_x_sum
+        self._y = exp_x / exp_x_sum
 
-    def backward(self, *args, **kwargs) -> np.ndarray:
-        raise NotImplementedError
+        return self._y
 
+    def backward(self, dy: np.ndarray) -> np.ndarray:
+        rank = len(self._y.shape)
+        batch = self._y.shape[:-1]
+        n = self._y.shape[-1]
+        
+        # https://towardsdatascience.com/derivative-of-the-softmax-function-and-the-categorical-cross-entropy-loss-ffceefc081d1
+        # Jacobian
+        # dy_i/dx_j = y_i(1{i=j} - y_j)
+        # Broadcasting batch dimensions and last 2 dimensions as Jacobian. 
+        j = np.expand_dims(np.eye(n), axis=tuple(range(rank - 1)))
+        j = j - np.expand_dims(self._y, axis=rank-1)
+        j = j * np.expand_dims(self._y, axis=rank)
+        return np.einsum('...a,...ba->...b', dy, j)
 
 class MultiHeadAttention(layer.StatefulLayer):
     def __init__(self, num_heads: int, *args, **kwargs):
@@ -87,6 +100,11 @@ class MultiHeadAttention(layer.StatefulLayer):
         if value is None:
             value = key
 
+        self._query = query
+        self._key = key
+        self._value = value
+        self._mask = mask
+
         batch = query.shape[0]
         if mask:
             assert mask.shape == (batch, self._num_heads, self._seq_len_q,
@@ -96,27 +114,111 @@ class MultiHeadAttention(layer.StatefulLayer):
         # [batch, seq_len_q, num_heads, key_dim]
         q = np.einsum(input_projection_equation, query, self._wq)
         q += self._bq
+        self._q = q
         # [batch, seq_len_kv, num_heads, key_dim]
         k = np.einsum(input_projection_equation, key, self._wk)
         k += self._bk
+        self._k = k
         # [batch, seq_len_kv, num_heads, value_dim]
         v = np.einsum(input_projection_equation, value, self._wv)
         v += self._bv
+        self._v = v
 
         # [batch, num_heads, seq_len_q, seq_len_kv]
         attention = np.einsum('...abc,...dbc->...bad', q, k)
         scaled_attention = (1.0 / np.sqrt(self._key_dim)) * attention
+        self._mask = mask
         if mask:
             scaled_attention = np.where(mask, scaled_attention, float('-inf'))
         attention_scores = self._softmax(scaled_attention)
 
-        # [batch, num_heads, seq_len_q, value_dim]
+        # values: [batch, num_heads, seq_len_q, value_dim]
+        self._attention_scores = attention_scores
         values = np.einsum('...abc,...cad->...abd', attention_scores, v)
 
+        self._attention_values = values
+        # wo: [num_heads * key_dim, num_heads, value_dim]
         o = np.einsum('...abc,...dac->...bd', values, self._wo)
         o += self._bo
 
+        # [batch, seq_len_q, num_heads * key_dim]
         return o
 
-    def backward(self, *args, **kwargs) -> np.ndarray:
-        raise NotImplementedError
+    def backward(
+            self, dy: np.ndarray,
+            learning_rate: float) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+        batch = dy.shape[0]
+
+        # Output projection
+        dbo = np.sum(dy, axis=(0, 1))
+        # values: [batch, num_heads, seq_len_q, value_dim]
+        # w:      [num_heads * key_dim, num_heads, value_dim]
+        # y:      [batch, seq_len_q, num_heads * key_dim]
+        dwo = np.sum(np.einsum('...abc,...bd->...dac', self._attention_values,
+                               dy),
+                     axis=0)
+        dvalues = np.einsum('...ab,bcd->...cad', dy, self._wo)
+
+        # Softmax@V
+        # attention_scores: [batch, num_heads, seq_len_q, seq_len_kv]
+        # v:                [batch, seq_len_kv, num_heads, value_dim]
+        # values:           [batch, num_heads, seq_len_q, value_dim]
+        assert dvalues.shape == (batch, self._num_heads, self._seq_len_q,
+                                 self._value_dim)
+        assert self._v.shape == (batch, self._seq_len_kv, self._num_heads,
+                                 self._value_dim)
+        dscores = np.einsum('...abc,...dac->...abd', dvalues, self._v)
+        dv = np.einsum('...abc,...abd->...cad', self._attention_scores,
+                       dvalues)
+
+        dscaled_attention = self._softmax.backward(dscores)
+
+        if self._mask:
+            raise NotImplementedError
+
+        dattention = dscaled_attention / np.sqrt(self._key_dim)
+
+        # Q^T@K
+        # attention: [batch, num_heads, seq_len_q, seq_len_kv]
+        # q:         [batch, seq_len_q, num_heads, key_dim]
+        # k:         [batch, seq_len_kv, num_heads, key_dim]
+        dq = np.einsum('...abc,...cad->...bad', dattention, self._k)
+        dk = np.einsum('...abc,...bad->...dbc', self._q, dattention)
+
+        # query: [batch, seq_len_q, num_heads * key_dim]
+        # wq:    [num_heads, key_dim, num_heads * key_dim]
+        # q:     [batch, seq_len_q, num_heads, key_dim]
+        input_projection_grad_w_equation = '...ab,...acd->...cdb'
+        input_projection_grad_x_equation = '...abc,bcd->...ad'
+        dwq = np.sum(np.einsum(input_projection_grad_w_equation, self._query,
+                               dq),
+                     axis=0)
+        dquery = np.einsum(input_projection_grad_x_equation, dq, self._wq)
+        dwk = np.sum(np.einsum(input_projection_grad_w_equation, self._key,
+                               dk),
+                     axis=0)
+        dkey = np.einsum(input_projection_grad_x_equation, dk, self._wk)
+        assert self._value.shape == (batch, self._seq_len_kv,
+                                     self._num_heads * self._value_dim)
+        assert dv.shape == (batch, self._seq_len_kv, self._num_heads,
+                            self._value_dim)
+        dwv = np.sum(np.einsum(input_projection_grad_w_equation, self._value,
+                               dv),
+                     axis=0)
+        dvalue = np.einsum(input_projection_grad_x_equation, dv, self._wv)
+
+        dbq = np.sum(dq, axis=(0, 1))
+        dbk = np.sum(dk, axis=(0, 1))
+        dbv = np.sum(dv, axis=(0, 1))
+
+        self._wq -= learning_rate * dwq
+        self._bq -= learning_rate * dbq
+        self._wk -= learning_rate * dwk
+        self._bk -= learning_rate * dbk
+        self._wv -= learning_rate * dwv
+        self._bv -= learning_rate * dbv
+        self._wo -= learning_rate * dwo
+        self._bo -= learning_rate * dbo
+
+        return dquery, dkey, dvalue
--- attention_test.py
+++ attention_test.py
@@ -1,5 +1,6 @@
 # Attention layer test
 
+import copy
 import unittest
 
 import collections.abc
@@ -15,6 +16,12 @@ def _rand(shape) -> np.ndarray:
     return np.random.normal(size=shape).astype(np.float32)
 
 
+@jax.jit
+def _mse_loss(y, targets):
+    diff = y - targets
+    return jnp.sum(diff**2) / y.size
+
+
 def _nested_printer(nested):
     if isinstance(nested, jnp.ndarray):
         return f'shape:{nested.shape}'
@@ -26,13 +33,40 @@ def _nested_printer(nested):
     return f'{nested}'
 
 
-class AttentionTest(unittest.TestCase):
-    def assert_allclose(self, lhs, rhs, *, rtol=1e-6, atol=1e-6):
+class NNTestCase(unittest.TestCase):
+    def assert_allclose(self, lhs, rhs, *, rtol=1e-5, atol=2e-5):
         return np.testing.assert_allclose(lhs, rhs, rtol=rtol, atol=atol)
 
-    def test_forward(self):
+
+class SotmaxTest(NNTestCase):
+    def test_forward_and_backward(self):
         np.random.seed(0)
-        
+
+        shape = [128, 128]
+        x = _rand(shape)
+        targets = _rand(shape)
+
+        @jax.jit
+        def flax_forward(x, targets):
+            y = flax.linen.softmax(x)
+            return _mse_loss(y, targets)
+
+        flax_grad = jax.jit(jax.grad(flax_forward))
+        # print(flax_grad.lower(x, targets).as_text())
+        flax_dx = flax_grad(x, targets)
+
+        softmax = attention.Softmax()
+        y = softmax.forward(x)
+        dy = jax.grad(_mse_loss)(y, targets)
+        dx = softmax.backward(dy)
+
+        self.assert_allclose(flax_dx, dx, rtol=1e-5, atol=1e-5)
+
+
+class AttentionTest(NNTestCase):
+    def test_forward_and_backward(self):
+        np.random.seed(0)
+
         batch = 16
         seq_len_q = 32
         seq_len_kv = 128
@@ -51,7 +85,7 @@ class AttentionTest(unittest.TestCase):
         #   value:{kernel:shape:(128, 8, 16), bias:shape:(8, 16)},
         #   out:{kernel:shape:(8, 16, 128), bias:shape:(128,)}}}
         # print(_nested_printer(variables))
-
+        # print(jax.tree_util.tree_map(lambda x : x.shape, variables))
         layer = attention.MultiHeadAttention(num_heads=num_heads)
         # Initialize layer
         output = layer(query=query)
@@ -59,8 +93,8 @@ class AttentionTest(unittest.TestCase):
         wq = np.transpose(variables['params']['query']['kernel'], [1, 2, 0])
         wk = np.transpose(variables['params']['key']['kernel'], [1, 2, 0])
         wv = np.transpose(variables['params']['value']['kernel'], [1, 2, 0])
-        wo = np.transpose(variables['params']['out']['kernel'], [2, 0, 1]) 
-        
+        wo = np.transpose(variables['params']['out']['kernel'], [2, 0, 1])
+
         bq = variables['params']['query']['bias']
         bk = variables['params']['key']['bias']
         bv = variables['params']['value']['bias']
@@ -70,12 +104,12 @@ class AttentionTest(unittest.TestCase):
         self.assertEqual(layer._wk.shape, wk.shape)
         self.assertEqual(layer._wv.shape, wv.shape)
         self.assertEqual(layer._wo.shape, wo.shape)
-        
+
         self.assertEqual(layer._bq.shape, bq.shape)
         self.assertEqual(layer._bk.shape, bk.shape)
         self.assertEqual(layer._bv.shape, bv.shape)
         self.assertEqual(layer._bo.shape, bo.shape)
-        
+
         layer._wq = wq
         layer._wk = wk
         layer._wv = wv
@@ -87,6 +121,52 @@ class AttentionTest(unittest.TestCase):
 
         output = layer(query)
         flax_output = flax_attention.apply(variables, query)
-       
+
         self.assert_allclose(output, flax_output)
 
+        targets = _rand([batch, seq_len_q, qkv_features])
+
+        @jax.jit
+        def flax_forward(variables, query, targets):
+            y = flax_attention.apply(variables, query)
+            return _mse_loss(y, targets)
+
+        flax_backward = jax.jit(jax.grad(flax_forward, argnums=(0, 1)))
+        # print(flax_backward.lower(variables, query, targets).as_text())
+
+        flax_grad = flax_backward(variables, query, targets)
+        # print(jax.tree_util.tree_map(lambda x : x.shape, flax_output))
+
+        flax_dwq = np.transpose(flax_grad[0]['params']['query']['kernel'],
+                                [1, 2, 0])
+        flax_dwk = np.transpose(flax_grad[0]['params']['key']['kernel'],
+                                [1, 2, 0])
+        flax_dwv = np.transpose(flax_grad[0]['params']['value']['kernel'],
+                                [1, 2, 0])
+        flax_dwo = np.transpose(flax_grad[0]['params']['out']['kernel'],
+                                [2, 0, 1])
+
+        flax_dbq = flax_grad[0]['params']['query']['bias']
+        flax_dbk = flax_grad[0]['params']['key']['bias']
+        flax_dbv = flax_grad[0]['params']['value']['bias']
+        flax_dbo = flax_grad[0]['params']['out']['bias']
+
+        flax_dquery = flax_grad[1]
+
+        learning_rate = 0.01
+
+        dy = jax.grad(_mse_loss)(output, targets)
+
+        # Makes a deepcopy as the backward path is going to update weights.
+        layer = copy.deepcopy(layer)
+        dquery, dkey, dvalue = layer.backward(dy, learning_rate)
+
+        self.assert_allclose(flax_dquery, dquery + dkey + dvalue)
+        self.assert_allclose(layer._wo, -learning_rate * flax_dwo + wo)
+        self.assert_allclose(layer._bo, -learning_rate * flax_dbo + bo)
+        self.assert_allclose(layer._wv, -learning_rate * flax_dwv + wv)
+        self.assert_allclose(layer._bv, -learning_rate * flax_dbv + bv)
+        self.assert_allclose(layer._wk, -learning_rate * flax_dwk + wk)
+        self.assert_allclose(layer._bk, -learning_rate * flax_dbk + bk)
+        self.assert_allclose(layer._wq, -learning_rate * flax_dwq + wq)
+        self.assert_allclose(layer._bq, -learning_rate * flax_dbq + bq)
